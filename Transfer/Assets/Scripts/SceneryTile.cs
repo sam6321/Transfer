8bbf054e270455523a3144b7d51bcaa0fff5a118
sam6321 <sam6321@live.com.au>
2019-06-30 20:24:40 +0800

    Added basic tile checking to robots

---
+++ Transfer/Assets/Scripts/SceneryTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneryTile : Tile
+{
+    public override void Use(Robot user)
+    {
+        // do nothing
+    }
+}
