b8f1bc6ee07b04cf802f2eab9abd1499e131abdb
sam6321 <sam6321@live.com.au>
2019-07-11 21:18:13 +0800

    Added lever up and down sounds

---
+++ Transfer/Assets/Scripts/LeverSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip onDownSound;
+
+    [SerializeField]
+    private AudioClip onUpSound;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOnDownSound()
+    {
+        audioSource.PlayOneShot(onDownSound);
+    }
+
+    public void PlayOnUpSound()
+    {
+        audioSource.PlayOneShot(onUpSound);
+    }
+}
